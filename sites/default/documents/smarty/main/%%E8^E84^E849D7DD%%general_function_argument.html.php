8b8ac9f0de5e6005a8c075d9d0fae089d5affcda
gustavoOtazu <gustavootazu2010@hotmail.com>
2021-08-22 11:27:29 -0400

    formulario para enfermeria

---
+++ sites/default/documents/smarty/main/%%E8^E84^E849D7DD%%general_function_argument.html.php
@@ -0,0 +1,3 @@
+<?php /* Smarty version 2.6.31, created on 2021-03-04 01:11:26
+         compiled from C:/xampp/htdocs/openemr3/templates/error/general_function_argument.html */ ?>
+You request could not be processed, an error occured with the number of arguments passed to the function. Please try again with corrected arguments.
